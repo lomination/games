dd52d53e288451574c21f132585d04a8652d45f8
lomination <lomination@rejna.org>
2026-05-14 15:52:16 +0000

    Write precondition in doc for Player.getMove

--- src/main/scala/lomination/games/core/players/Player.scala
+++ src/main/scala/lomination/games/core/players/Player.scala
@@ -5,4 +5,11 @@ import lomination.games.core.shared.Move
 
 trait Player[G <: ZeroSumGame[G, M], M <: Move]:
 
+  /** Note: this function assumes that there is at least one possible move in
+    * the current state of the game, i.e. `game.legalMoves` should return a
+    * non-empty sequence.
+    *
+    * @param game
+    * @return
+    */
   def getMove(game: G): Option[M]
