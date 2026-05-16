927a8c8711d802964fb9c14ee6d041f8455760a7
lomination <lomination@rejna.org>
2026-05-16 07:07:45 +0000

    Add empty seq guard in choose extension

--- src/main/scala/lomination/games/core/shared/Extensions.scala
+++ src/main/scala/lomination/games/core/shared/Extensions.scala
@@ -3,4 +3,6 @@ package lomination.games.core.shared
 // Helper to pick a random element if multiple moves have the same score
 extension [T](seq: Seq[T])
   def choose: T =
-    seq(scala.util.Random.nextInt(seq.size))
+    if seq.isEmpty then
+      throw new IllegalStateException("Cannot choose from an empty collection.")
+    else seq(scala.util.Random.nextInt(seq.size))
